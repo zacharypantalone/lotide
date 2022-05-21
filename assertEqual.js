6d17a0f3cfd0a3f1f235a7fcd4cc5698e53ac017
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-05-21 10:26:02 -0400

    assignment complete

--- assertEqual.js
+++ assertEqual.js
@@ -1,1 +1,11 @@
-console.log(testin123);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑Assertion Failed🛑: [${actual}] !== [${expected}]`);
+  }
+  
+};
+
+assertEqual("lighthouse Labs", "lighthouse Labs");
+assertEqual(1, 1);
