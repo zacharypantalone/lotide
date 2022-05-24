8538726d13384fb1cdb7c43b2650182fb7b67761
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-05-24 16:36:54 -0400

    complete

--- tail.js
+++ tail.js
@@ -30,5 +30,5 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 console.log(assertEqualArray(result, ["Lighthouse", "Labs"]));
 
 
-// Create a function tail which returns 
+// Create a function tail which returns
 // +the "tail" of an array: everything except for the first item (head) of the provided array.
