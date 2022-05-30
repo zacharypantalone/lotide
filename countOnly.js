98b54334f26a6ca47835093a6dbeccfc7b8cc4a8
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-05-30 12:47:11 -0400

    complete

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑Assertion Failed🛑: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let resultObject = {};
+
+  for (let items of allItems) {
+    if (itemsToCount[items]) {
+      
+      if (resultObject[items]) {
+        resultObject[items] += 1;
+      } else {
+        resultObject[items] = 1;
+      }
+    }
+  }
+  return resultObject;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
