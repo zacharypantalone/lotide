e898e5efa8c7067b73bc8973e2a47e898eda1934
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-05-30 16:16:02 -0400

    complete

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  //this is the happy path
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (!result) {
+    console.log(`🛑Assertion Failed🛑: [${arr1}] !== [${arr2}]`);
+    return;
+  }
+
+  console.log(`✅Assertion Passed✅: [${arr1}] === [${arr2}]`);
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (!results.hasOwnProperty[letter]) {
+        results[letter] = [];
+      }
+    }
+  }
+  for (let i = 0; i < sentence.length; i++) {
+    if (results.hasOwnProperty(sentence[i])) {
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+let results = letterPositions("lighthouse in the house");
+assertArraysEqual(results.l, [0]);
+
+console.log(results);
