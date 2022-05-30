4c7048a4e1caca2c95ff52b73d6825ce891a8256
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-05-30 13:42:25 -0400

    complete

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑Assertion Failed🛑: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let resultObject = {};
+
+  for (let letters of sentence) {
+    if (letters !== " ") {
+      if (resultObject[letters]) {
+        resultObject[letters] += 1;
+      } else {
+        resultObject[letters] = 1;
+      }
+    }
+  }
+  return resultObject;
+};
+
+console.log(countLetters("lighthouse in the house"));
