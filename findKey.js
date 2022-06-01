c859aae7b4327a43d7a81278b0a417599de9a357
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-05-31 20:17:50 -0400

    complete

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed✅: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑Assertion Failed🛑: [${actual}] !== [${expected}]`);
+  }
+  
+};
+
+//assertEqual("lighthouse Labs", "lighthouse Labs");
+//assertEqual(1, 1);
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key]) === true) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
