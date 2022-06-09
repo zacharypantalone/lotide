e20dadb08dd7d35b2b21b093768155247523652a
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-08 22:13:58 -0400

    assertArraysEqual.js refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  //this is the happy path
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 //treat if like a filter, check for what you DON'T want
 
@@ -24,5 +12,8 @@ const assertArraysEqual = function(arr1, arr2) {
   console.log(`✅Assertion Passed✅: [${arr1}] === [${arr2}]`);
 };
 
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+module.exports = assertArraysEqual;
