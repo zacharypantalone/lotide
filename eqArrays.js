d628d4d9b21127bd20db8c0697682af82aa782a6
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-08 21:52:11 -0400

    eqArrays.js refactored

--- eqArrays.js
+++ eqArrays.js
@@ -12,5 +12,6 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+
+module.exports = eqArrays;
